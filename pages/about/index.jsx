2481500151e2908b14e68c0b1faa9c8d6d267fe1
Tuppers360 <tuppers360@live.co.uk>
2020-10-05 20:38:22 +0100

    create about page

---
+++ pages/about/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "./../../components/layout/index";
+
+function About() {
+  return (
+    <Layout>
+      <div>About Page</div>
+    </Layout>
+  );
+}
+
+export default About;
