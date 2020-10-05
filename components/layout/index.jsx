d343669acd9d7c0f397ef94c5d65b5e1b50a3fe1
Tuppers360 <tuppers360@live.co.uk>
2020-10-05 20:38:03 +0100

    add link for about page

--- components/layout/index.jsx
+++ components/layout/index.jsx
@@ -23,7 +23,7 @@ const Layout = ({ children, title = "Flex App" }) => (
           <a href="#">Home</a>
         </li>
         <li>
-          <a href="#">About</a>
+          <a href="/about">About</a>
         </li>
         <li>
           <a href="#">Services</a>
