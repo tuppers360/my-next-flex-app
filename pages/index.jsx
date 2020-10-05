bf8a3d175c8d8d0638c6610ab4b2c8f3db7733ad
Tuppers360 <tuppers360@live.co.uk>
2020-10-05 20:38:48 +0100

    add about area and styles

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,3 @@
-import Head from "next/head";
 import styles from "../styles/Home.module.scss";
 import Layout from "./../components/layout/index";
 
@@ -12,6 +11,25 @@ export default function Home() {
         <a href="#" className={styles.bannerbutton}>
           Contact Us
         </a>
+      </section>
+
+      <section className={styles.aboutarea} id="about">
+        <h3 className={styles.sectiontitle}>About Us</h3>
+        <ul className={styles.aboutcontent}>
+          <li className={styles.aboutleft}></li>
+          <li className={styles.aboutright}>
+            <h2>About Our Company</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Doloremque, dolorum saepe libero laboriosam aliquam hic dolorem
+              maxime rerum in. Aperiam et alias, ullam tenetur voluptatum
+              voluptates ipsam molestiae quidem iure.
+            </p>
+            <a href="" className={styles.aboutbtn}>
+              Learn more
+            </a>
+          </li>
+        </ul>
       </section>
     </Layout>
   );
