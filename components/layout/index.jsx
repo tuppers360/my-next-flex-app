1b2448ca75844a2282ea62fbd18afa3c5c482a65
tuppers360 <tuppers360@live.co.uk>
2020-10-05 10:37:12 +0100

    add new header and banner section

--- components/layout/index.jsx
+++ components/layout/index.jsx
@@ -1,15 +1,38 @@
 import React from "react";
 import Head from "next/head";
-import Nav from "../nav";
+import styles from "./layout.module.scss";
 
-const Layout = ({ children, title = "My Next Flex App" }) => (
+const Layout = ({ children, title = "Flex App" }) => (
   <>
     <Head>
       <title>{title}</title>
       <link rel="icon" href="/favicon.ico" />
       <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Merienda:wght@700&display=swap"
+        rel="stylesheet"
+      ></link>
     </Head>
+    <header>
+      <h2>
+        <a href="#">Flex App</a>
+      </h2>
+      <nav>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Services</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </nav>
+    </header>
     {children}
     <footer></footer>
   </>
